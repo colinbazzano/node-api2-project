6c9f1fee653584c324484fc5fca203bd297343f6
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-03 18:40:41 -0500

    comment is done

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const Posts = require("../data/db");
 
+// POST A POST
 router.post("/", (req, res) => {
   const postData = req.body;
 
@@ -25,6 +26,7 @@ router.post("/", (req, res) => {
     });
 });
 
+// POST A COMMENT
 router.post("/:id/comments", (req, res) => {
   const id = req.params.id;
   const commentData = req.body;
@@ -58,12 +60,13 @@ router.post("/:id/comments", (req, res) => {
         console.log(error);
         res.status(500).json({
           message:
-            "There was an erroe while saving the comment to the database."
+            "There was an error while saving the comment to the database."
         });
       });
   }
 });
 
+// GET POSTS
 router.get("/", (req, res) => {
   Posts.find(req.query)
     .then(post => {
@@ -77,6 +80,7 @@ router.get("/", (req, res) => {
     });
 });
 
+// GET POST BY ID
 router.get("/:id", (req, res) => {
   const id = req.params.id;
 
@@ -84,11 +88,9 @@ router.get("/:id", (req, res) => {
     .then(post => {
       post
         ? res.status(200).json(post)
-        : res
-            .status(404)
-            .json({
-              message: "The post with the specified ID does not exist."
-            });
+        : res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
     })
     .catch(error => {
       console.log(error);
@@ -97,5 +99,90 @@ router.get("/:id", (req, res) => {
         .json({ error: "The post information could not be retrieved." });
     });
 });
+
+router.get("/:id/comments", (req, res) => {
+  const id = req.params.id;
+
+  Posts.findById(id)
+    .then(comment => {
+      comment
+        ? res.status(200).json(comment)
+        : res.status(404).json({
+            message: "The  with the specified ID does not exist."
+          });
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+router.get("/:id/comments", (req, res) => {
+  const id = req.params.id;
+  const commentData = req.body;
+
+  Posts.findPostComments(id)
+    .then(comment => {
+      comment.length > 0
+        ? res.status(200).json(commentData.text)
+        : Posts.findById(id)
+            .then(post => {
+              if (post.length > 0) {
+                res.status(404).json({
+                  message:
+                    "The post with the specified ID does not contain comments."
+                });
+              } else {
+                res.status(404).json({
+                  message: "The post with the specified ID does not exist."
+                });
+              }
+            })
+            .catch(error => {
+              console.log(error);
+              res.status(500).json({
+                error: "The post information could not be retrieved. "
+              });
+            });
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+// router.get("/:id/comments", (req, res) => {
+//   const id = req.params.id;
+//   const commentData = req.body;
+//   Posts.findById(id)
+//     .then(post => {
+//       if (post.length !== 0) {
+//         Posts.findPostComments(commentData)
+//           .then(comment => {
+//             res.status(201).json(...comment.text);
+//           })
+//           .catch(error => {
+//             console.log(error);
+//             res.status(500).json({
+//               error:
+//                 "There was an error while saving the comment to the database."
+//             });
+//           });
+//       } else {
+//         res
+//           .status(404)
+//           .json({ message: "The post with specified ID does not exist." });
+//       }
+//     })
+//     .catch(error => {
+//       console.log(error);
+//       res.status(500).json({
+//         message: "There was an error while saving the comment to the database."
+//       });
+//     });
+// });
 
 module.exports = router;
