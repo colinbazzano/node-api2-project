9c9e18f558bc61ab400b87e8d4ac8e29ab5a9fad
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-03 17:35:15 -0500

    finished finding a post by its id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -77,4 +77,25 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Posts.findById(id)
+    .then(post => {
+      post
+        ? res.status(200).json(post)
+        : res
+            .status(404)
+            .json({
+              message: "The post with the specified ID does not exist."
+            });
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
 module.exports = router;
